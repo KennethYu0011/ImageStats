977ca4f3051af060d017c9be833545fb67b82dd0
KennTheCodingSaiyan <jiananyu0011@gmail.com>
2018-11-30 11:35:33 -0500

    final version

--- ImageStats.java
+++ ImageStats.java
@@ -1,1 +1,75 @@
-../src/main/java/ImageStats.java
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ImageStats {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable rowSum = new IntWritable();
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer line = new StringTokenizer(value.toString(),"\n\r");
+      while (line.hasMoreTokens()) {
+        System.out.println("=======================================");
+        String[] words = line.nextToken().split("\t", 6);
+
+        for(String s:words){
+          System.out.println(s);
+        }
+        int sum = Integer.valueOf(words[2]) +
+                  Integer.valueOf(words[3]) +
+                  Integer.valueOf(words[4]);
+        word.set(words[1]);
+        rowSum.set(sum);
+        context.write(word, rowSum);
+      }
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Image Stats");
+    job.setJarByClass(ImageStats.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
